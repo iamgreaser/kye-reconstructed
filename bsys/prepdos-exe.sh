7ceee07bea0909a849d8263f41ecded3b0645f3d
GreaseMonkey <thematrixeatsyou+cobederg@gmail.com>
2025-04-07 18:50:43 +1200

    More progress! 2 more mouse things to go and this first one will be really interesting as I finally have to deal with stack frames of more than one variable...

--- bsys/prepdos-exe.sh
+++ bsys/prepdos-exe.sh
@@ -5,5 +5,5 @@ C_OBJ_LIST=""
 for C_NAME in $@; do
   C_OBJ_LIST="${C_OBJ_LIST} D:\\BUILD\\${C_NAME}.OBJ"
 done
-echo "bcc -k- -G -O2 -1 -W -ms -M -LC:\\BORLANDC\\LIB -eD:\\OUT\\KYE.EXE ${C_OBJ_LIST}"
+echo "bcc -O2 -a -1 -W -ms -M -LC:\\BORLANDC\\LIB -eD:\\OUT\\KYE.EXE ${C_OBJ_LIST}"
 echo "rc D:\\SRC\\KYE.RES D:\\OUT\\KYE.EXE"
