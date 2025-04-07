dd6b87d145122cf16500650e63b79b331a390b91
GreaseMonkey <thematrixeatsyou+cobederg@gmail.com>
2025-04-07 15:26:51 +1200

    Split off WndProc into its own file
    This seems to be one of the files anyway

--- bsys/prepdos-exe.sh
+++ bsys/prepdos-exe.sh
@@ -3,7 +3,7 @@ set -e
 
 C_OBJ_LIST=""
 for C_NAME in $@; do
-  C_OBJ_LIST="${C_OBJS} D:\\BUILD\\${C_NAME}.OBJ"
+  C_OBJ_LIST="${C_OBJ_LIST} D:\\BUILD\\${C_NAME}.OBJ"
 done
 echo "bcc -W -ms -LC:\\BORLANDC\\LIB -eD:\\OUT\\KYE.EXE ${C_OBJ_LIST}"
 echo "rc D:\\SRC\\KYE.RES D:\\OUT\\KYE.EXE"
