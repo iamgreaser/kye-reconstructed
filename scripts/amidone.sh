44a0d35434c2635a2e51357a0a22aa2759e2c747
GreaseMonkey <thematrixeatsyou+cobederg@gmail.com>
2025-04-07 20:47:12 +1200

    Use hex byte instead of canonical for amidone.sh

--- scripts/amidone.sh
+++ scripts/amidone.sh
@@ -2,6 +2,6 @@
 # amidone.sh: Do we have a 100% match?
 set -e
 
-hexdump -Cv ref/20/KYE.EXE >build/matchref.txt
-hexdump -Cv out/KYE.EXE >build/matchnew.txt
+hexdump -Xv ref/20/KYE.EXE >build/matchref.txt
+hexdump -Xv out/KYE.EXE >build/matchnew.txt
 vimdiff build/matchref.txt build/matchnew.txt
