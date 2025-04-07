6f1cd0f9c684c221c441bfd72cc2ae992a341046
GreaseMonkey <thematrixeatsyou+cobederg@gmail.com>
2025-04-07 16:54:20 +1200

    ACTUALLY START MATCHING CODE!

--- bsys/prepdos-obj.sh
+++ bsys/prepdos-obj.sh
@@ -4,5 +4,5 @@ set -e
 for C_NAME in $@; do
   C_SRC="D:\\SRC\\${C_NAME}.CPP"
   C_OBJ="D:\\BUILD\\${C_NAME}.OBJ"
-  echo "bcc -W -ms -IC:\\BORLANDC\\INCLUDE -ID:\\SRC -c -o${C_OBJ} ${C_SRC}"
+  echo "bcc -k- -G -O2 -1 -W -ms -IC:\\BORLANDC\\INCLUDE -ID:\\SRC -c -o${C_OBJ} ${C_SRC}"
 done
