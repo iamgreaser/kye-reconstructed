c55d2096feef0c7d31a79729a09c1ba08462b2bd
GreaseMonkey <thematrixeatsyou+cobederg@gmail.com>
2025-04-08 20:01:35 +1200

    Add and match more code; Start a new module split; Use a response file for filenames

--- bsys/prepdos-exe.sh
+++ bsys/prepdos-exe.sh
@@ -5,5 +5,6 @@ C_OBJ_LIST=""
 for C_NAME in $@; do
   C_OBJ_LIST="${C_OBJ_LIST} D:\\BUILD\\${C_NAME}.OBJ"
 done
-echo "bcc -O2 -y -1 -G -W -ms -M -LC:\\BORLANDC\\LIB -eD:\\OUT\\KYE.EXE ${C_OBJ_LIST}"
+  echo "-O2 -y -1 -G -W -ms -M -LC:\\BORLANDC\\LIB -eD:\\OUT\\KYE.EXE ${C_OBJ_LIST}" >build/MKEXE.ARG
+echo "bcc @D:\\BUILD\\MKEXE.ARG"
 echo "rc D:\\SRC\\KYE.RES D:\\OUT\\KYE.EXE"
