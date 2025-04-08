8459ad76c5a5e5688acc6131e1825fe5387432ff
GreaseMonkey <thematrixeatsyou+cobederg@gmail.com>
2025-04-08 16:50:33 +1200

    Make amidone.sh much more useful

--- scripts/amidone.sh
+++ scripts/amidone.sh
@@ -2,6 +2,8 @@
 # amidone.sh: Do we have a 100% match?
 set -e
 
-hexdump -Xv ref/20/KYE.EXE >build/matchref.txt
-hexdump -Xv out/KYE.EXE >build/matchnew.txt
+#hexdump -Xv ref/20/KYE.EXE >build/matchref.txt
+#hexdump -Xv out/KYE.EXE >build/matchnew.txt
+ndisasm -b16 -e0x0600 -o0x0000 ref/20/KYE.EXE >build/matchref.txt
+ndisasm -b16 -e0x0600 -o0x0000 out/KYE.EXE >build/matchnew.txt
 vimdiff build/matchref.txt build/matchnew.txt
