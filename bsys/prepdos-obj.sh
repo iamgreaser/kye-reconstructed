097f4b3ac8b5b7a5877651fd6e1bc22d2a9f824f
GreaseMonkey <thematrixeatsyou+cobederg@gmail.com>
2025-04-09 13:30:32 +1200

    Take the nuclear option and special-case compiler flags for UNK004.CPP to make the first function match

--- bsys/prepdos-obj.sh
+++ bsys/prepdos-obj.sh
@@ -4,6 +4,14 @@ set -e
 for C_NAME in $@; do
   C_SRC="D:\\SRC\\${C_NAME}.CPP"
   C_OBJ="D:\\BUILD\\${C_NAME}.OBJ"
-  echo "bcc -O2 -1 -W -ms -IC:\\BORLANDC\\INCLUDE -ID:\\SRC -c -o${C_OBJ} ${C_SRC}"
+  case "${C_NAME}" in
+    UNK004)
+      echo "bcc -O2 -O-v -1 -W -ms -IC:\\BORLANDC\\INCLUDE -ID:\\SRC -c -o${C_OBJ} ${C_SRC}"
+      ;;
+    *)
+      echo "bcc -O2 -1 -W -ms -IC:\\BORLANDC\\INCLUDE -ID:\\SRC -c -o${C_OBJ} ${C_SRC}"
+      ;;
+  esac
+
   echo "if errorlevel 1 goto fail"
 done
