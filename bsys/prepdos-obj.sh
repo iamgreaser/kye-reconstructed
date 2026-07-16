bdaa32266a45c1da678eb828d58cfa7aa31cb197
GreaseMonkey <thematrixeatsyou+cobederg@gmail.com>
2026-07-16 12:55:58 +1200

    Finish down-casing things, because I forgot to do it for the unk004.c special case

--- bsys/prepdos-obj.sh
+++ bsys/prepdos-obj.sh
@@ -5,7 +5,7 @@ for C_NAME in $@; do
   C_SRC="D:\\SRC\\${C_NAME}.C"
   C_OBJ="D:\\BUILD\\${C_NAME}.OBJ"
   case "${C_NAME}" in
-    UNK004)
+    unk004)
       # convert_char_to_internal() needs -Ov disabled in order to match (induction variable generation)
       echo "bcc -O2 -O-v -1 -W -ms -IC:\\BORLANDC\\INCLUDE -ID:\\SRC -c -o${C_OBJ} ${C_SRC}"
       ;;
