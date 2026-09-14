9b90628ea60837cf627c1bd0cdeb3e21ff113b2a
GreaseMonkey <thematrixeatsyou+cobederg@gmail.com>
2026-09-14 19:34:59 +1200

    Add more ordinals to relocscan and actually factor in which library each symbol is from

--- scripts/relocscan.py
+++ scripts/relocscan.py
@@ -5,10 +5,14 @@ from __future__ import annotations
 import struct, sys
 
 IDMAP: dict[int, str] = {
-    0x0022: "BitBlt",
-    0x002D: "SelectObject",
-    0x0034: "CreateCompatibleDC",
-    0x0044: "DeleteDC",
+    0x0001001B: "Rectangle",
+    0x0001001F: "SetPixel",
+    0x00010021: "TextOut",
+    0x00010022: "BitBlt",
+    0x0001002D: "SelectObject",
+    0x00010033: "CreateCompatibleBitmap",
+    0x00010034: "CreateCompatibleDC",
+    0x00010044: "DeleteDC",
 }
 
 def main() -> None:
@@ -17,7 +21,7 @@ def main() -> None:
         reloc_count, = struct.unpack("<H", fp.read(2))
         for irel in range(reloc_count):
             rtyp, rflg, woffs, rseg, roffs = struct.unpack("<BBHHH", fp.read(8))
-            knownid = IDMAP.get(roffs, "?")
+            knownid = IDMAP.get(roffs|(rseg<<16), "?")
             #print(f"{irel:04X}: {rtyp:02X} {rflg:02X} {woffs:04X} {rseg:04X}:{roffs:04X} {knownid}")
             print(f"{rtyp:02X} {rflg:02X} {woffs:04X} {rseg:04X}:{roffs:04X} {knownid}")
     return
