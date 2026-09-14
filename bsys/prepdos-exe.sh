38d3b3c09aee247fdf6cfa533d3686730395cc71
GreaseMonkey <thematrixeatsyou+cobederg@gmail.com>
2026-09-14 12:52:28 +1200

    Tweaks to build scripts and stuff as I try to make sense of how the entry table and module name are determined

--- bsys/prepdos-exe.sh
+++ bsys/prepdos-exe.sh
@@ -6,25 +6,35 @@ for C_NAME in $@; do
   C_OBJ_LIST="${C_OBJ_LIST} D:\\BUILD\\${C_NAME}.OBJ"
 done
 
-if false; then
+if true; then
   # Link via BCC
   echo "-y -M -O2 -1 -W -ms -LC:\\BORLANDC\\LIB -eD:\\OUT\\KYE.EXE ${C_OBJ_LIST}" >build/MKEXE.ARG
   echo "bcc @D:\\BUILD\\MKEXE.ARG"
 else
   # Link with TLINK directly
+  # NOTE: Probably not used unless there's a way to make 6-byte entry table entries appear.
   LIB_LIST=""
   for LIB_NAME in CWS.LIB IMPORT.LIB; do
     LIB_LIST="${LIB_LIST} C:\\BORLANDC\\LIB\\${LIB_NAME}"
   done
-  echo "/Twe C:\\BORLANDC\\LIB\\C0WS.OBJ ${C_OBJ_LIST},D:\\OUT\\KYE.EXE,D:\\OUT\\KYE.MAP,${LIB_LIST},D:\\SRC\\KYE.DEF" >build/MKEXE.ARG
+  echo "/Twe C:\\BORLANDC\\LIB\\C0WS.OBJ ${C_OBJ_LIST},D:\\OUT\\Kye,D:\\OUT\\KYE.MAP,${LIB_LIST},D:\\SRC\\KYE.DEF" >build/MKEXE.ARG
   echo "tlink @D:\\BUILD\\MKEXE.ARG"
 fi
 
 echo "if errorlevel 1 goto fail"
 echo "rc -?"
-echo "cd D:\\SRC\\"
-echo "rc -r -i C:\\BORLANDC\\INCLUDE -fo D:\\BUILD\\KYE.RES D:\\SRC\\KYE.RC"
-echo "if errorlevel 1 goto fail"
-echo "cd \\"
-echo "rc D:\\BUILD\\KYE.RES D:\\OUT\\KYE.EXE"
-echo "if errorlevel 1 goto fail"
+if true; then
+  # Compile resource separately from binding to the EXE
+  echo "cd D:\\SRC\\"
+  echo "rc -r -i C:\\BORLANDC\\INCLUDE -fo D:\\BUILD\\KYE.RES D:\\SRC\\KYE.RC"
+  echo "if errorlevel 1 goto fail"
+  echo "cd \\"
+  echo "rc D:\\BUILD\\KYE.RES D:\\OUT\\KYE.EXE"
+  echo "if errorlevel 1 goto fail"
+else
+  # Compile resource directly into EXE
+  # NOTE: Probably not used, I suspect Borland's tools were used to generate a .RES file directly.
+  echo "cd D:\\SRC\\"
+  echo "rc -i C:\\BORLANDC\\INCLUDE D:\\SRC\\KYE.RC D:\\OUT\\KYE.EXE"
+  echo "if errorlevel 1 goto fail"
+fi
--- scripts/amidone.sh
+++ scripts/amidone.sh
@@ -2,10 +2,15 @@
 # amidone.sh: Do we have a 100% match?
 set -e
 
-#hexdump -Xv ref/20/KYE.EXE >build/matchref.txt
-#hexdump -Xv out/KYE.EXE >build/matchnew.txt
-hexdump -Cv ref/20/KYE.EXE >build/matchref.txt
-hexdump -Cv out/KYE.EXE >build/matchnew.txt
-#ndisasm -b16 -e0x0600 -o0x0000 ref/20/KYE.EXE >build/matchref.txt
-#ndisasm -b16 -e0x0600 -o0x0000 out/KYE.EXE >build/matchnew.txt
+if false; then
+  # Hex dump
+  #hexdump -Xv ref/20/KYE.EXE >build/matchref.txt
+  #hexdump -Xv out/KYE.EXE >build/matchnew.txt
+  hexdump -Cv ref/20/KYE.EXE >build/matchref.txt
+  hexdump -Cv out/KYE.EXE >build/matchnew.txt
+else
+  # Disassembly
+  ndisasm -b16 -e0x0600 -o0x0000 ref/20/KYE.EXE >build/matchref.txt
+  ndisasm -b16 -e0x0600 -o0x0000 out/KYE.EXE >build/matchnew.txt
+fi
 vimdiff build/matchref.txt build/matchnew.txt
