458284ac6d5586b3c288ee710bfa584599642786
GreaseMonkey <thematrixeatsyou+cobederg@gmail.com>
2026-09-14 19:17:35 +1200

    Add script for showing the code segment relocations and use that in amidone.sh in the meantime

--- scripts/amidone.sh
+++ scripts/amidone.sh
@@ -2,12 +2,16 @@
 # amidone.sh: Do we have a 100% match?
 set -e
 
-if true; then
+if false; then
   # Hex dump
   #hexdump -Xv ref/20/KYE.EXE >build/matchref.txt
   #hexdump -Xv out/KYE.EXE >build/matchnew.txt
   hexdump -Cv ref/20/KYE.EXE >build/matchref.txt
   hexdump -Cv out/KYE.EXE >build/matchnew.txt
+elif true; then
+  # Relocation dump (assumes relocations are at 0x890B!)
+  ./scripts/relocscan.py ref/20/KYE.EXE >build/matchref.txt
+  ./scripts/relocscan.py out/KYE.EXE >build/matchnew.txt
 else
   # Disassembly
   ndisasm -b16 -e0x0600 -o0x0000 ref/20/KYE.EXE >build/matchref.txt
---
+++ scripts/relocscan.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
+import struct, sys
+
+IDMAP: dict[int, str] = {
+    0x0022: "BitBlt",
+    0x002D: "SelectObject",
+    0x0034: "CreateCompatibleDC",
+    0x0044: "DeleteDC",
+}
+
+def main() -> None:
+    with open(sys.argv[1], "rb") as fp:
+        fp.seek(0x890B)  # hardcoded start of CODE relocs / end of CODE image octets
+        reloc_count, = struct.unpack("<H", fp.read(2))
+        for irel in range(reloc_count):
+            rtyp, rflg, woffs, rseg, roffs = struct.unpack("<BBHHH", fp.read(8))
+            knownid = IDMAP.get(roffs, "?")
+            #print(f"{irel:04X}: {rtyp:02X} {rflg:02X} {woffs:04X} {rseg:04X}:{roffs:04X} {knownid}")
+            print(f"{rtyp:02X} {rflg:02X} {woffs:04X} {rseg:04X}:{roffs:04X} {knownid}")
+    return
+
+if __name__ == "__main__":
+    main()
