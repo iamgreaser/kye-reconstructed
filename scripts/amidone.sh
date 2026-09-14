03f0d3c796c93235d3d78620fa04dd782f94503b
GreaseMonkey <thematrixeatsyou+cobederg@gmail.com>
2026-09-14 19:21:29 +1200

    Make the amidone mode selectable by an argument

--- scripts/amidone.sh
+++ scripts/amidone.sh
@@ -2,19 +2,29 @@
 # amidone.sh: Do we have a 100% match?
 set -e
 
-if false; then
-  # Hex dump
-  #hexdump -Xv ref/20/KYE.EXE >build/matchref.txt
-  #hexdump -Xv out/KYE.EXE >build/matchnew.txt
-  hexdump -Cv ref/20/KYE.EXE >build/matchref.txt
-  hexdump -Cv out/KYE.EXE >build/matchnew.txt
-elif true; then
-  # Relocation dump (assumes relocations are at 0x890B!)
-  ./scripts/relocscan.py ref/20/KYE.EXE >build/matchref.txt
-  ./scripts/relocscan.py out/KYE.EXE >build/matchnew.txt
-else
-  # Disassembly
-  ndisasm -b16 -e0x0600 -o0x0000 ref/20/KYE.EXE >build/matchref.txt
-  ndisasm -b16 -e0x0600 -o0x0000 out/KYE.EXE >build/matchnew.txt
-fi
-vimdiff build/matchref.txt build/matchnew.txt
+case $1 in
+  hex)
+    # Hex dump
+    #hexdump -Xv ref/20/KYE.EXE >build/matchref.txt
+    #hexdump -Xv out/KYE.EXE >build/matchnew.txt
+    hexdump -Cv ref/20/KYE.EXE >build/matchref.txt
+    hexdump -Cv out/KYE.EXE >build/matchnew.txt
+    vimdiff build/matchref.txt build/matchnew.txt
+    ;;
+  reloc)
+    # Relocation dump (assumes relocations are at 0x890B!)
+    ./scripts/relocscan.py ref/20/KYE.EXE >build/matchref.txt
+    ./scripts/relocscan.py out/KYE.EXE >build/matchnew.txt
+    vimdiff build/matchref.txt build/matchnew.txt
+    ;;
+  asm)
+    # Disassembly
+    ndisasm -b16 -e0x0600 -o0x0000 ref/20/KYE.EXE >build/matchref.txt
+    ndisasm -b16 -e0x0600 -o0x0000 out/KYE.EXE >build/matchnew.txt
+    vimdiff build/matchref.txt build/matchnew.txt
+    ;;
+  *)
+    echo "INVALID ARGUMENT - provide a single argument of one of: asm hex reloc"
+    false
+    ;;
+esac
