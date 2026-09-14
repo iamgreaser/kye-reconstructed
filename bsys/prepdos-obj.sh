af828cf5c25786e93fa79f2e7551cdfd671e6cd3
GreaseMonkey <thematrixeatsyou+cobederg@gmail.com>
2026-09-14 19:13:10 +1200

    Add line numbers to map file

--- bsys/prepdos-obj.sh
+++ bsys/prepdos-obj.sh
@@ -7,10 +7,10 @@ for C_NAME in $@; do
   case "${C_NAME}" in
     #unk004)
     #  # convert_char_to_internal() needs -Ov disabled in order to match (induction variable generation)
-    #  echo "bcc -O2 -O-v -1 -W -ms -IC:\\BORLANDC\\INCLUDE -ID:\\SRC -c -o${C_OBJ} ${C_SRC}"
+    #  echo "bcc -v -O2 -O-v -1 -W -ms -IC:\\BORLANDC\\INCLUDE -ID:\\SRC -c -o${C_OBJ} ${C_SRC}"
     #  ;;
     *)
-      echo "bcc -O2 -1 -W -ms -IC:\\BORLANDC\\INCLUDE -ID:\\SRC -c -o${C_OBJ} ${C_SRC}"
+      echo "bcc -v -O2 -1 -W -ms -IC:\\BORLANDC\\INCLUDE -ID:\\SRC -c -o${C_OBJ} ${C_SRC}"
       ;;
   esac
 
